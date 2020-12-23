563c5d0b8650c3a87505655c4ff5b7f0e6fdf40b
caroline4790 <q.sun4790@gmail.com>
2020-12-23 18:44:10 -0500

    quick sort

---
+++ java/Recursion/sort_laicode_10_quick_sort.java
@@ -0,0 +1,44 @@
+public class sort_laicode_10_quick_sort {
+    public int[] quickSort(int[] array) {
+        // Write your solution here
+        if (array == null || array.length <= 1) {
+            return array;
+        }
+        quickSort(array, 0, array.length - 1);
+        return array;
+    }
+
+    private void quickSort(int[] array, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int pivotPos = partition(array, left, right);
+        quickSort(array, left, pivotPos - 1);
+        quickSort(array, pivotPos + 1, right);
+    }
+
+    private int partition(int[] array, int left, int right) {
+        int pivotIndex= left + (right - left) / 2;
+        int pivot = array[pivotIndex];
+        swap(array, pivotIndex, right);
+        int leftBound = left;
+        int rightBound = right - 1;
+        while (leftBound <= rightBound) {
+            if (array[leftBound] < pivot) {
+                leftBound++;
+            } else if (array[rightBound] >= pivot) {
+                rightBound--;
+            } else {
+                swap(array, leftBound++, rightBound--);
+            }
+        }
+        swap(array, leftBound, right);
+        return leftBound;
+    }
+
+    private void swap(int[] array, int left, int right) {
+        int temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+    }
+}
